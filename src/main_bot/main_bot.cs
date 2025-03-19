cabcd5725a1756a5a3f1fe0a6b7dab6ae3b853e4
Diyah Susan <diyahnugrahani04@gmail.com>
2025-03-19 18:29:11 +0700

    fix : fix onHitBot algo

--- src/main_bot/main_bot.cs
+++ src/main_bot/main_bot.cs
@@ -3,6 +3,12 @@ using System.Drawing;
 using Robocode.TankRoyale.BotApi;
 using Robocode.TankRoyale.BotApi.Events;
 
+// BOT INFINITEA
+// Bot utama kelompok infinitea, menggabungkan beberapa pendekatan algoritma greedy dari bot alternatif
+// Menggunakan gerakan dengan pola 8 terinspirasi dari nomor kelompok 8 infinitie
+// Menggunakan strategi tembak berdasarkan jarak dan sudut dari bot musuh
+// Berusaha menghindar apabila energi tidak mencukupi
+
 public class main_bot : Bot
 {
     int turnDirection = 1;   
@@ -23,6 +29,7 @@ public class main_bot : Bot
 
         SetTurnRight(360);
 
+        // jalan dengan pola 8
         while (IsRunning)
         {
             
@@ -31,7 +38,6 @@ public class main_bot : Bot
             {
                 SetTurnRight(45);
                 Forward(70);
-                // TurnGunRight(45);
             }
             
             // second loop, pola 8 (counter-clockwise)    
@@ -39,27 +45,21 @@ public class main_bot : Bot
             {
                 SetTurnLeft(45);
                 Forward(70);
-                // TurnGunLeft(45);
             }
 
 
         }
     }
 
-    // We saw another bot -> fire!
     public override void OnScannedBot(ScannedBotEvent e)
     {        
         var bearing = BearingTo(e.X, e.Y);
         var distance = DistanceTo(e.X, e.Y);
 
-        // Fire depending on angle and distance
+        // menembak based on jarak dan sudut
         if (distance > -100 && distance < 100 && Math.Abs(bearing) <= 10){
             TurnLeft(bearing);
-            Fire(Math.Min(3, Energy-0.1));
-        }
-        else if (Math.Abs(bearing) <= 10){
-            TurnLeft(bearing);
-            Fire(Math.Min(2, Energy-0.1));
+            Fire(Math.Min(4, Energy-0.1));
         }
         else if (Math.Abs(distance) <= 500){
             TurnLeft(bearing);
@@ -72,39 +72,37 @@ public class main_bot : Bot
         }
     }
 
+    // kalau energi mencukupi maka tembak
     public override void OnHitBot(HitBotEvent e)
     {
-        Console.WriteLine("Ouch! I hit a bot at " + e.X + ", " + e.Y);
         var distance = DistanceTo(e.X,e.Y);
         
-        if (Energy < 30){
-            Back(50);
-            TurnRight(45);
+        if (Energy < 25){
+            TurnRight(90);
+            Forward(100);
         } else {
             TurnToFaceTarget(e.X,e.Y);
             Fire(4);
         }
     }
 
+    // putar balik saat menabrak tembok
     public override void OnHitWall(HitWallEvent e)
     {
-        Console.WriteLine("Ouch! I hit a wall, must turn back!");
         TurnRight(180);
         Forward(50);
     }
 
-    // We were hit by a bullet -> turn perpendicular to the bullet
+    // kalau ketembak, menghindar dengan berbelok
     public override void OnHitByBullet(HitByBulletEvent evt)
     {
         TurnRight(90);
         Forward(100);
-        // // Calculate the bearing to the direction of the bullet
-        // var bearing = CalcBearing(evt.Bullet.Direction);
-
-        // // Turn 90 degrees to the bullet direction based on the bearing
-        // TurnLeft(90 - bearing);
+    
     }
-    /* Read the documentation for more events and methods */
+
+
+    // menghadap ke musuh
     private void TurnToFaceTarget(double x, double y)
     {
         var bearing = BearingTo(x, y);
